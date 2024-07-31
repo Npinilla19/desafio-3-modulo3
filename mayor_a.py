0a78da28feb9dacb62e480811005031654dd4b3a
Vanessa Morales N <vane.moralesn@gmail.com>
2024-07-30 21:09:54 -0400

    se realiza modificación para la incorporación de libreria SYS
    se incorpora sys.argv , para cumplir con la solicitud de que se debe ingresar carpeta seguida del valor

--- mayor_a.py
+++ mayor_a.py
@@ -1,20 +1,17 @@
+import sys 
 ventas = {
     "Enero": 15000, "Febrero": 22000, "Marzo": 12000, "Abril": 17000, "Mayo": 81000, "Junio": 13000, "Julio": 21000, "Agosto": 41200, "Septiembre": 25000, 
     "Octubre": 21500, "Noviembre": 91000, "Diciembre": 21000,
 }
 
-Venta_mas_alta = max(ventas.values())
+if len(sys.argv) != 2:  
+    print("Por favor, ingrese un umbral como argumento en la línea de comandos.")  
+    sys.exit(1)  
 
-while True:
-    try:
-        monto = int(input("Ingrese un monto: "))
-        if monto > Venta_mas_alta:
-            print("Lo siento, el monto ingresado es superior al monto más alto en las ventas.")
-            
-        else:
-            # Crear un nuevo diccionario que contiene solo los meses y ventas que son mayores al monto ingresado
-            Respuesta = {mes: venta for mes, venta in ventas.items() if venta > monto}
-            print(Respuesta)
-            break  # Salir del bucle si se ha ingresado un monto válido
-    except ValueError:
-        print("Por favor, ingrese un número entero válido.")
+try:  
+    umbral = int(sys.argv[1])   
+    resultado = {mes: venta for mes, venta in ventas.items() if venta > umbral}   
+    print(resultado)  
+
+except ValueError:  
+    print("Por favor, ingrese un número entero válido como argumento.")
